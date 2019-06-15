08937601ad779d2ffe90b6892b863dcc2fd98bb5
defi2015 <11107040@qq.com>
2019-06-15 12:46:52 +0800

    增加跨域支持

--- activate-web/src/main/java/com/order/machine/ActivateWebApplication.java
+++ activate-web/src/main/java/com/order/machine/ActivateWebApplication.java
@@ -2,12 +2,12 @@ package com.order.machine;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import tk.mybatis.spring.annotation.MapperScan;
 
 @SpringBootApplication
 @MapperScan(basePackages = "com.order.machine.mapper")
 public class ActivateWebApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(ActivateWebApplication.class, args);
 	}
